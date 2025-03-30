056fb8fa23241a624bbd47c040791a48af7cee11
Jerome Doran <disneyfan9184@gmail.com>
2025-03-30 12:52:07 -0400

    Add utility functions and UI components with Radix UI integration

---
+++ data/description.js
@@ -0,0 +1,290 @@
+export const descriptionData = [
+  {
+    id: 'apple',
+    name: 'Apple',
+  },
+  {
+    id: 'pcc-airfoils',
+    name: 'PCC Airfoils',
+  },
+  {
+    id: 'huntington',
+    name: 'Huntington Bank',
+  },
+  {
+    id: 'target',
+    name: 'Target',
+  },
+  {
+    id: 'mikolich-orthadontics',
+    name: 'Mikolich Orthadontics',
+  },
+  {
+    id: 'verizon',
+    name: 'Verizon',
+  },
+  {
+    id: 'nationwide-life',
+    name: 'Nationwide Life Insurance',
+  },
+  {
+    id: 'paypal',
+    name: 'Paypal',
+  },
+  {
+    id: 'aer',
+    name: 'Aer',
+  },
+  {
+    id: 'alpaka',
+    name: 'Alpaka',
+  },
+  {
+    id: 'amazon',
+    name: 'Amazon',
+  },
+  {
+    id: 'armstrong',
+    name: 'Armstrong',
+  },
+  {
+    id: 'bmo-bank',
+    name: 'BMO Bank',
+  },
+  {
+    id: 'chase',
+    name: 'Chase Bank',
+  },
+  {
+    id: 'dicks-sporting-goods',
+    name: "Dick's Sporting Goods",
+  },
+  {
+    id: 'dollar-general',
+    name: 'Dollar General',
+  },
+  {
+    id: 'dollar-tree',
+    name: 'Dollar Tree',
+  },
+  {
+    id: 'dunkin',
+    name: 'Dunkin',
+  },
+  {
+    id: 'fgl',
+    name: 'FGL Mortgage Insurance',
+  },
+  {
+    id: 'giant-eagle',
+    name: 'Giant Eagle',
+  },
+  {
+    id: 'jc-penney',
+    name: 'JC Penney',
+  },
+  {
+    id: 'marcs',
+    name: "Marc's",
+  },
+  {
+    id: 'marshals',
+    name: 'Marshals',
+  },
+  {
+    id: 'nationwide-car',
+    name: 'Nationwide Car Insurance',
+  },
+  {
+    id: 'premier-bank',
+    name: 'Premier Bank',
+  },
+  {
+    id: 'springfield-grill',
+    name: 'Springfield Grill',
+  },
+  {
+    id: 'disney',
+    name: 'Disney',
+  },
+  {
+    id: 'sandals-beach-resort',
+    name: 'Sandals Beach Resort',
+  },
+  {
+    id: 'stonebridge',
+    name: 'Stonebridge',
+  },
+  {
+    id: 'us-treasury-department',
+    name: 'US Treasury Department',
+  },
+  {
+    id: 'ohio-department-of-taxation',
+    name: 'Ohio Department of Taxation',
+  },
+  {
+    id: 'tj-maxx',
+    name: 'TJ Maxx',
+  },
+  {
+    id: 'youtube-tv',
+    name: 'Youtube TV',
+  },
+  {
+    id: 'casals',
+    name: 'Casals',
+  },
+  {
+    id: 'us-post-office',
+    name: 'US Post Office',
+  },
+  {
+    id: 'regal-cinemas',
+    name: 'Regal Cinemas',
+  },
+  {
+    id: 'ebay',
+    name: 'Ebay',
+  },
+  {
+    id: 'chilis',
+    name: 'Chilis',
+  },
+  {
+    id: 'initial-income-balance',
+    name: 'Initial Income Balance',
+  },
+  {
+    id: 'sparkle',
+    name: 'Sparkle',
+  },
+  {
+    id: 'enbridge',
+    name: 'Enbridge',
+  },
+  {
+    id: 'first-energy',
+    name: 'First Energy',
+  },
+  {
+    id: 'youngstown-water',
+    name: 'Youngstown Water',
+  },
+  {
+    id: 'subway',
+    name: 'Subway',
+  },
+  {
+    id: 'meijers',
+    name: 'Meijers',
+  },
+  {
+    id: 'golden-star-cinemas',
+    name: 'Golden Star Cinemas',
+  },
+  {
+    id: 'aldi',
+    name: 'Aldi',
+  },
+  {
+    id: 'canes',
+    name: 'Raising Canes',
+  },
+  {
+    id: 'kohls',
+    name: 'Kolhs',
+  },
+  {
+    id: 'quest-diagnostics',
+    name: 'Quest Diagnostics',
+  },
+  {
+    id: 'chipotle',
+    name: 'Chipotle',
+  },
+  {
+    id: 'allison',
+    name: 'Allison Brothers',
+  },
+  {
+    id: 'millcreek-gc',
+    name: 'Millcreek Golf Course',
+  },
+  {
+    id: 'sports-clips',
+    name: 'Sport Clips',
+  },
+  {
+    id: 'discover card',
+    name: 'Discover Card',
+  },
+  {
+    id: 'discover-bank',
+    name: 'Discover Bank',
+  },
+  {
+    id: 'walmart',
+    name: 'Walmart',
+  },
+  {
+    id: 'sams-club',
+    name: "Sam's Club",
+  },
+  {
+    id: 'macys',
+    name: 'Macys',
+  },
+  {
+    id: 'speedway',
+    name: 'Speedway',
+  },
+  {
+    id: 'walgreens',
+    name: 'Walgreens',
+  },
+  {
+    id: 'driveway-company',
+    name: 'The Driveway Company',
+  },
+  {
+    id: 'rescue-mission',
+    name: 'The Rescue Mission',
+  },
+  {
+    id: 'maze-craze',
+    name: 'Maze Craze',
+  },
+  {
+    id: 'handyman-hardware',
+    name: 'Handyman Hardware',
+  },
+  {
+    id: 'elmton',
+    name: 'Elmton',
+  },
+  {
+    id: 'namecheap',
+    name: 'Name Cheap',
+  },
+  {
+    id: 'mcdonalds',
+    name: 'McDonalds',
+  },
+  {
+    id: 'philadelphia-candies',
+    name: 'Philadelphia Candies',
+  },
+  {
+    id: 'ruli',
+    name: 'Rulli Brothers',
+  },
+  {
+    id: 'sbarros',
+    name: 'Sbarros',
+  },
+  {
+    id: 'five-below',
+    name: 'Five Below',
+  },
+];
